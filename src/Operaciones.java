4ef634671b265533908b1e8260f5f03f54383a1a
Mik-69 <mikhailzatonsky@gmail.com>
2024-12-03 22:02:30 +0100

    Añadido Operaciones.java en rama FuncionesBasicas

---
+++ src/Operaciones.java
@@ -0,0 +1,15 @@
+public class Operaciones {
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 5;
+        int resultado1, resultado2;
+
+        resultado1 = num1 * num2;
+        resultado2 = num1 / num2;
+
+        System.out.println("La multiplicación de " + num1 + " x " + num2 + " es: " + resultado1);
+        System.out.println("La división de " + num1 + " / " + num2 + " es: " + resultado2);
+
+    }
+}
